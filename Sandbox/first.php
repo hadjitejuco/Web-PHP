354aae778566cd2beaa97e722c22e78167fa957e
Hadji J. Tejuco <hadji.tejuco@gmail.com>
2019-01-27 14:23:59 +0800

    Add files via upload
    first.php

---
+++ Sandbox/first.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First PHP Script</title>
+</head>
+<body>
+    <?php
+        echo 'This is my first PHP scirpt';
+
+    ?>
+</body>
+</html>
